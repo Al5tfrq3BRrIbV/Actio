62e95e560c875fa94c605e080bff3fbe55355318
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-16 21:46:58 +0100

    m

--- server/code/ActioObject.js
+++ server/code/ActioObject.js
@@ -11,8 +11,8 @@ function ToDoList(category) {
 	this.category = category;
 	this.list = [];
 	this.readToDoList = function(dataIn) {
+		console.log(JSON.parse(dataIn));
 		JSON.parse(dataIn).forEach(function (toDoIn) {
-			console.log(this);
 			this.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user));
 		});
 		console.log("Interpreting toDoList");
