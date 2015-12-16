73e009283a0b3d7afda9e7931372c6314830c1e8
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-16 23:50:38 +0100

    m

--- server/code/ActioObject.js
+++ server/code/ActioObject.js
@@ -14,7 +14,7 @@ function ToDoList(category) {
 		console.log(this);
 		console.log(JSON.parse(dataIn));
 		JSON.parse(dataIn).forEach( $.proxy( function (toDoIn) {
-			this.a.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user));
+			this.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user));
 		}), this.a);
 		console.log("Interpreting toDoList");
 		this.b(this.list, ".toDoList1");
