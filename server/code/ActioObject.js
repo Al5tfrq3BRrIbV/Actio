48a4e3339973422edb442d05fb042c43576f065d
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-16 23:47:12 +0100

    m

--- server/code/ActioObject.js
+++ server/code/ActioObject.js
@@ -11,7 +11,7 @@ function ToDoList(category) {
 	this.category = category;
 	this.list = [];
 	this.readToDoList = function(dataIn) {
-		console.log(dataIn);
+		console.log(this);
 		console.log(JSON.parse(dataIn));
 		JSON.parse(dataIn).forEach( $.proxy( function (toDoIn) {
 			this.a.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user));
