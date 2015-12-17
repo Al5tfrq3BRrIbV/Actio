35c382ac29eeb3a3ceef78cc352be91872d6c18a
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-17 22:15:42 +0100

    m

--- server/code/actio.js
+++ server/code/actio.js
@@ -14,7 +14,7 @@ var main = function() {
 	toDoList1.list.push(secondTodo);
 
 	getToDoList(toDoList1, ".toDoList1");
-
+	toDoList1.list.push(firstTodo);
 
 	console.log(toDoList1.checkExists(firstTodo.id));
 };
