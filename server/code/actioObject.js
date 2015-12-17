cbf9dffd2873f9064fdc947b814deb8791cb61dc
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-17 22:21:07 +0100

    m

--- server/code/actioObject.js
+++ server/code/actioObject.js
@@ -18,11 +18,13 @@ function ToDoList(category) {
 		}), this.toDoList);
 		this.display(this.toDoList, this.location);
 	}
-	this.checkExists = $.proxy(function(id) {
-		console.log(this);
-		for(i=0;i<this.list.length;i++){
-			if(this.list.indexOf(i).id === id) return true;
-		}
-		return false;
-	}, this);
+	this.checkExists = function(id){
+		return $.proxy(function(id) {
+			console.log(this);
+			for(i=0;i<this.list.length;i++){
+				if(this.list[i]   === id) return true;
+			}
+			return false;
+		}, this);
+	}
 }
