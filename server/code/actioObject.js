081bb4587ea776ba90a6e8ce11439a3695eb2468
Al5tfrq3BRrIbV <heuveltje@hotmail.com>
2015-12-17 22:34:05 +0100

    m

--- server/code/actioObject.js
+++ server/code/actioObject.js
@@ -13,8 +13,9 @@ function ToDoList(category) {
 	this.list = [];
 	this.readToDoList = function(dataIn) {
 		JSON.parse(dataIn).forEach( $.proxy( function (toDoIn) {
-
-			this.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user, toDoIn.category));
+			if(!this.checkExists(toDoIn.id)) {
+				this.list.push(new ToDo(toDoIn.id, toDoIn.text, toDoIn.priority, toDoIn.dueDate, toDoIn.done, toDoIn.user, toDoIn.category));
+			}
 		}), this.toDoList);
 		this.display(this.toDoList, this.location);
 	}
