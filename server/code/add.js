3b0b20fe29f7a0fdff8a9ce167a3426615c3f0b2
Yinghao Dai <yinghaodai@hotmail.com>
2015-12-18 14:45:35 +0100

    y

--- server/code/add.js
+++ server/code/add.js
@@ -2,6 +2,7 @@ var add = function(event) {
 	if($("input.title").val() && $("input.date").val() && $("input.category").val()){
 		var newToDo = new ToDo(undefined,$("input.title").val(),$("input[name=priority]:checked").val(),$("input.date").val(),0,"user1",$("input.category").val());
 		uploadToDo(newToDo);
+		window.location.replace("index.html");
 	}
 };
 
